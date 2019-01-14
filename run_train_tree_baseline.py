954a1bf320747f12145647ee226c352393c93e3f
Danny Martinez <33183828+Dnndmt@users.noreply.github.com>
2019-01-14 21:36:53 +0100

    Code to run baseline
    The code will run the baseline models with the Decision Tree framework. The baseline is defined as using the previous day target variables to predict the next day targets.

--- run_train_tree_baseline.py
+++ run_train_tree_baseline.py
@@ -6,14 +6,14 @@ tgt_names = ['Returns', 'Volatility']
 feat_lst = [sr_targets.iloc[-3152:-1,:], vola_targets.iloc[:-1,:]]
 feat_names = ['Returns_pr', 'Volatility_pr']
 
-data_acc1 = {}
-data_f11 = {}
-data_best_p1 = {}
+data_acc_pr = {}
+data_f1_pr = {}
+data_best_p_pr = {}
 for i, tgt in enumerate(tgt_lst):
     for j, feat in enumerate(feat_lst):
      
-        acc_results1, f1_results1, best_p_lst1 = train_tree(feat, tgt)
+        acc_results_pr, f1_results_pr, best_p_lst_pr = train_tree(feat, tgt)
         
-        data_acc1['{0} & {1}'.format(feat_names[j], tgt_names[i])] = acc_results1
-        data_f11['{0} & {1}'.format(feat_names[j], tgt_names[i])] = f1_results1
-        data_best_p1['{0} & {1}'.format(feat_names[j], tgt_names[i])] = best_p_lst1
+        data_acc_pr['{0} & {1}'.format(feat_names[j], tgt_names[i])] = acc_results_pr
+        data_f1_pr['{0} & {1}'.format(feat_names[j], tgt_names[i])] = f1_results_pr
+        data_best_p_pr['{0} & {1}'.format(feat_names[j], tgt_names[i])] = best_p_lst_pr
